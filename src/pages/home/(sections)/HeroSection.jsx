a75650d4cd3630f869c3533aeda18851f2eebe07
hrithikscube <hrithik@scube.me>
2023-09-13 13:02:33 +0530

    banner carousel

--- src/pages/home/(sections)/HeroSection.jsx
+++ src/pages/home/(sections)/HeroSection.jsx
@@ -1,3 +1,4 @@
+import BannerCarousel from '@/components/common/BannerCarousel'
 import React from 'react'
 
 const categories = [
@@ -14,12 +15,18 @@ const categories = [
 
 const HeroSection = () => {
     return (
-        <div className='lg:w-[1290px] m-auto flex flex-col lg:my-10 my-5'>
-            <div className='flex flex-col items-start gap-4  text-start '>
+        <div className='lg:w-[1290px] m-auto flex flex-row justify-between lg:my-10 my-5'>
+
+            <div className='flex flex-col items-start gap-4  text-start w-3/12'>
                 {React.Children.toArray(categories.map(item => (
                     <p className='text-base font-poppins font-medium lg:text-lg'>{item}</p>
                 )))}
             </div>
+
+            <div className='w-9/12'>
+                <BannerCarousel />
+            </div>
+
         </div>
     )
 }
