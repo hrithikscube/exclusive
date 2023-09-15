84ec8457ab637aaebbf60044ea00e26f635c5b24
hrithikscube <hrithik@scube.me>
2023-09-15 12:59:11 +0530

    remaining sections

--- src/pages/home/(sections)/Sections.jsx
+++ src/pages/home/(sections)/Sections.jsx
@@ -190,6 +190,32 @@ const Sections = () => {
                     </div>
                 </div>
 
+                <div className='my-5 lg:px-0 px-5'>
+
+                    <div className='flex lg:flex-row flex-col items-start justify-center lg:gap-10 gap-5'>
+
+                        <div className='flex flex-col items-center justify-center'>
+                            <img src="/icons/delivery.svg" alt="delivery" />
+                            <h2 className='font-poppins uppercase text-lg font-bold'>Free and Fast Delivery</h2>
+                            <p className='text-sm font-poppins font-medium'>Free delivery for all orders over $140</p>
+                        </div>
+
+                        <div className='flex flex-col items-center justify-center'>
+                            <img src="/icons/delivery.svg" alt="delivery" />
+                            <h2 className='font-poppins uppercase text-lg font-bold'>24/7 CUSTOMER SERVICE</h2>
+                            <p className='text-sm font-poppins font-medium'>Friendly 24/7 customer support</p>
+                        </div>
+
+                        <div className='flex flex-col items-center justify-center'>
+                            <img src="/icons/delivery.svg" alt="delivery" />
+                            <h2 className='font-poppins uppercase text-lg font-bold'>MONEY BACK GUARANTEE</h2>
+                            <p className='text-sm font-poppins font-medium'>We reurn money within 30 days</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
             </div>
 
         </Fragment>
